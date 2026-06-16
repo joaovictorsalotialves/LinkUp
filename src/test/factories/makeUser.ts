45aa8802ea51ac9be81524beca6e9e12337a08cd
João Victor <joaovictorsalotialves12@gmail.com>
2026-06-16 15:01:26 -0300

    fix: corrige a factory makeUser

--- src/test/factories/makeUser.ts
+++ src/test/factories/makeUser.ts
@@ -2,24 +2,17 @@ import type { UniqueEntityID } from '@/core/entities/value-objects/UniqueEntityI
 import type { Optional } from '@/core/utils/optional'
 import { User, type UserProps } from '@/domain/user/enterprise/entities/User'
 
-export const makeUser = (
-  user: Optional<UserProps, keyof UserProps> = {
-    username: 'JohnDoe',
-    email: 'john@example.com',
-    passwordHash: 'hash_password',
-  },
-  id?: UniqueEntityID
-): User => {
+export const makeUser = (user?: Optional<UserProps, keyof UserProps>, id?: UniqueEntityID): User => {
   return User.create(
     {
-      username: user.username as string,
-      email: user.email as string,
-      passwordHash: user.passwordHash as string,
-      bio: user.bio,
-      profilePhotoUrl: user.profilePhotoUrl,
-      status: user.status,
-      createdAt: user.createdAt,
-      updatedAt: user.updatedAt,
+      username: user?.username ?? 'JohnDoe',
+      email: user?.email ?? 'john@example.com',
+      passwordHash: user?.passwordHash ?? 'hash_password',
+      bio: user?.bio,
+      profilePhotoUrl: user?.profilePhotoUrl,
+      status: user?.status,
+      createdAt: user?.createdAt,
+      updatedAt: user?.updatedAt,
     },
     id
   )
